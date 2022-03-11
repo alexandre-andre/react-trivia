1947976c386bbb7e18ac108691626445be84fc75
Yuri Carvalho <yuricps@gmail.com>
2022-03-11 16:42:06 -0300

    Requisito 4 OK :heavy_check_mark:

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { string, number } from 'prop-types';
+
+class Header extends Component {
+  render() {
+    const { name, score, hash } = this.props;
+    return (
+      <header>
+        <img
+          src={ `https://www.gravatar.com/avatar/${hash}` }
+          alt={ `Foto de ${name}` }
+          data-testid="header-profile-picture"
+        />
+        <h4 data-testid="header-player-name">{name}</h4>
+        <p data-testid="header-score">{score}</p>
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = ({ userInfo: { name, score, hash } }) => ({
+  name, score, hash,
+});
+
+export default connect(mapStateToProps)(Header);
+
+Header.propTypes = {
+  name: string,
+  score: number,
+  hash: string,
+}.isRequired;
--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 // import PropType from 'prop-types';
 import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Game extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1>Pagina Game</h1>
       </div>
     );
