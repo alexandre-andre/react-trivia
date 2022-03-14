bac305dd68f02693b0474e6b504abed035911b0e
Yuri Carvalho <yuricps@gmail.com>
2022-03-14 19:19:41 -0300

    Requisito 8 OK :heavy_check_mark:

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -9,6 +9,9 @@ class Game extends Component {
   state = {
     questions: [],
     questionsIndex: 0,
+    answers: [],
+    timer: 30,
+    isDisabled: false,
   }
 
   componentDidMount = async () => {
@@ -24,13 +27,38 @@ class Game extends Component {
     if (triviaQuestions.length !== 0) {
       this.setState({ questions: triviaQuestions.results });
     }
+
+    const responseObj = triviaQuestions.results;
+    const answersArray = [];
+    responseObj.forEach((el) => { // Pega as alternativas para embaralhar
+      const answers = [];
+      el.incorrect_answers.map((answer, index) => (
+        answers.push({ answer, testId: `wrong-answer-${index}`, id: 'wrong-answer' })
+      ));
+      answers.push({
+        answer: el.correct_answer,
+        testId: 'correct-answer',
+        id: 'correct-answer' });
+      answersArray.push(answers);
+    });
+
+    answersArray.forEach((el) => this.shuffleArray(el));
+
+    this.setState({ answers: answersArray });
+
+    const oneSecond = 1000;
+    setInterval(this.decreaseTimer, oneSecond);
   }
 
   nextQuestion = () => {
     const { questionsIndex } = this.state;
     const limitQuestions = 3;
     if (questionsIndex <= limitQuestions) {
-      this.setState({ questionsIndex: questionsIndex + 1 });
+      this.setState({
+        questionsIndex: questionsIndex + 1,
+        timer: 30,
+        isDisabled: false,
+      });
     }
 
     this.removeAnswerColor();
@@ -55,29 +83,25 @@ class Game extends Component {
     }
   }
 
+  // Função abaixo baseada em https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+  shuffleArray = (array) => {
+    for (let k = array.length - 1; k > 0; k -= 1) {
+      const j = Math.floor(Math.random() * (k + 1));
+      [array[k], array[j]] = [array[j], array[k]];
+    }
+  }
+
+  decreaseTimer = () => {
+    const { timer } = this.state;
+    if (timer !== 0) this.setState({ timer: timer - 1 });
+
+    if (timer === 0) this.setState({ timer: 0, isDisabled: true });
+  }
+
   render() {
-    const { questions, questionsIndex: i } = this.state;
+    const { questions, questionsIndex: i, answers, timer, isDisabled } = this.state;
     const question = questions[i];
-    let answers = [];
-
-    // Função abaixo baseada em https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
-    function shuffleArray(array) {
-      for (let k = array.length - 1; k > 0; k -= 1) {
-        const j = Math.floor(Math.random() * (k + 1));
-        [array[k], array[j]] = [array[j], array[k]];
-      }
-    }
-
-    if (questions.length) {
-      answers = question.incorrect_answers.map((answer, index) => (
-        { answer, testId: `wrong-answer-${index}`, id: 'wrong-answer' }
-      ));
-      answers.push({
-        answer: question.correct_answer,
-        testId: 'correct-answer',
-        id: 'correct-answer' });
-      shuffleArray(answers);
-    }
+    const answer = answers[i];
 
     return (
       <div>
@@ -85,19 +109,20 @@ class Game extends Component {
         <h1>Pagina Game</h1>
         <div>
           {
-            questions.length
+            answers.length
             && (
               <>
                 <h4 data-testid="question-category">{ question.category }</h4>
                 <h4 data-testid="question-text">{ question.question }</h4>
                 <div data-testid="answer-options">
                   {
-                    answers.map((item, index) => (
+                    answer.map((item, index) => (
                       <button
                         key={ index }
                         type="button"
                         id={ `${item.id}` }
                         data-testid={ `${item.testId}` }
+                        disabled={ isDisabled }
                         onClick={ this.handleAnswerColor }
                       >
                         { item.answer }
@@ -109,6 +134,10 @@ class Game extends Component {
             )
           }
         </div>
+        <p>
+          Timer:
+          {`${timer}`}
+        </p>
         <br />
         <div>
           <button
